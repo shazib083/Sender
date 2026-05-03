7cc6637a4413af41799d9076b908667d3dc02374
Shazib <shazib@gmail.com>
2026-05-03 08:32:32 +0600

    fix deployment issue

--- next.config.js
+++ next.config.js
@@ -12,11 +12,8 @@ const nextConfig = {
       fs: false,
       net: false,
       tls: false,
-      crypto: require.resolve("crypto-browserify"),
-      stream: require.resolve("stream-browserify"),
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      os: require.resolve("os-browserify/browser"),
+      encoding: false,
+      "pino-pretty": false,
     };
     return config;
   },
